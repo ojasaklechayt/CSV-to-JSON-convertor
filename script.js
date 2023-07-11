e6254dd987686dc853b15528a13c3ffc0cd5c9d6
Ojas Aklecha <90605717+ojasaklechayt@users.noreply.github.com>
2023-07-11 22:57:33 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ function createtable(csvData) {
 
     headerRow.unshift("Serial Number");
 
-    let tableHTML = "<table>";
+    let tableHTML = "<table class='table'>";
     tableHTML += "<thead><tr>";
     for (let i = 0; i < numcols + 2; i++) {
         if (i === numcols + 1) {
@@ -39,14 +39,14 @@ function createtable(csvData) {
         console.log(cols);
         let rowSum = 0;
         tableHTML += "<tr>";
-        tableHTML += `<td>${i}</td>`;
+        tableHTML += `<td contentEditable>${i}</td>`;
         for (let j = 0; j < numcols; j++) {
             const cellValue = cols[j];
             if (isNaN(cellValue) && cellValue !== "") {
                 containsAlphabets = true;
                 window.alert("Presence of string value");
             }
-            tableHTML += `<td>${cols[j]}</td>`;
+            tableHTML += `<td contentEditable>${cols[j]}</td>`;
             rowSum += parseInt(cols[j], 10);
         }
         tableHTML += `<td>${rowSum.toFixed(2)}</td>`;
@@ -60,11 +60,20 @@ function createtable(csvData) {
     const tablecontainer = document.getElementById("showtable");
     tablecontainer.innerHTML = tableHTML;
 
+    // Add mx-auto class to center the table horizontally
+    const table = tablecontainer.querySelector("table");
+    table.classList.add("mx-auto");
+
     const addRowBtn = document.getElementById("add-row-btn");
     addRowBtn.addEventListener("click", addRow);
 
     const deleteRowBtn = document.getElementById("delete-row-btn");
     deleteRowBtn.addEventListener("click", deleteRow);
+
+    const cells = document.querySelectorAll('td[contentEditable="true"]');
+    cells.forEach(cell => {
+        cell.addEventListener("input", updateTotal);
+    });
 
     function addRow() {
         let sum = 0;
@@ -73,19 +82,23 @@ function createtable(csvData) {
 
         const newRow = document.createElement("tr");
         const newSerialNumberCell = document.createElement("td");
+        newSerialNumberCell.setAttribute("contentEditable", "true");
         newSerialNumberCell.textContent = numRows + 1;
         newRow.appendChild(newSerialNumberCell);
 
         for (let i = 0; i < numcols; i++) {
             const newCell = document.createElement("td");
-            newCell.textContent = 1;
+            newCell.setAttribute("contentEditable", "true");
+            newCell.textContent = "1";
+            sum += parseInt(newCell.textContent);
             newRow.appendChild(newCell);
         }
         const newTotalCell = document.createElement("td");
-        newTotalCell.textContent = "0.00";
+        newTotalCell.textContent = sum;
         newRow.appendChild(newTotalCell);
 
         tableBody.appendChild(newRow);
+        updateTotal();
     }
 
     function deleteRow() {
@@ -94,6 +107,26 @@ function createtable(csvData) {
         if (numRows > 0) {
             tableBody.removeChild(tableBody.lastChild);
         }
+        updateTotal();
+    }
+
+    function updateTotal() {
+        let total = 0;
+        const rows = document.querySelectorAll("tbody tr");
+        rows.forEach(row => {
+            const cells = row.querySelectorAll("td");
+            let rowSum = 0;
+            for (let i = 1; i < cells.length; i++) {
+                const cellValue = cells[i].textContent.trim();
+                if (!isNaN(cellValue) && cellValue !== "") {
+                    rowSum += parseInt(cellValue, 10);
+                }
+            }
+            cells[cells.length - 1].textContent = rowSum.toFixed(2);
+            total += rowSum;
+        });
+        const totalCell = document.querySelector("thead th:last-child");
+        totalCell.textContent = total.toFixed(2);
     }
 
     return tableHTML;
@@ -104,10 +137,9 @@ function exportCSV() {
     const rows = Array.from(table.querySelectorAll("tr"));
     const headerCells = Array.from(rows[0].querySelectorAll("th"));
     const headerRow = headerCells.map(cell => cell.textContent).join(",");
-    const dataCells = rows.slice(1).map(row =>
-        Array.from(row.querySelectorAll("td")).map(td => td.textContent)
-    );
-    const data = [headerRow, ...dataCells].join("");
+    const dataCells = rows.slice(1).map(row => Array.from(row.querySelectorAll("td")).map(td => td.textContent));
+    const dataRows = dataCells.map(row => row.join(","));
+    const data = [headerRow, ...dataRows].join("\n");
     const dataUri = "data:text/csv;charset=utf-8," + encodeURIComponent(data);
     const exportLink = document.createElement("a");
     exportLink.href = dataUri;
@@ -121,13 +153,9 @@ function exportJSON() {
     const table = document.querySelector("table");
     const rows = Array.from(table.querySelectorAll("tr"));
     const headerCells = Array.from(rows[0].querySelectorAll("th"));
-    const dataCells = rows.slice(1).map(row =>
-        Array.from(row.querySelectorAll("td")).map(td => td.textContent)
-    );
-    const data = dataCells.map(row =>
-        Object.fromEntries(headerCells.map((cell, i) => [cell.textContent, row[i]]))
-    );
-    const jsonData = JSON.stringify(data, null, 2);
+    const dataCells = rows.slice(1).map(row => Array.from(row.querySelectorAll("td")).map(td => td.textContent));
+    const data = dataCells.map(row => Object.fromEntries(headerCells.map((cell, i) => [cell.textContent, row[i]])));
+    const jsonData = JSON.stringify(data, null, null);
     const dataUri = "data:text/json;charset=utf-8," + encodeURIComponent(jsonData);
     const exportLink = document.createElement("a");
     exportLink.href = dataUri;
