b990f02dab430f7bacc5f0357cc017978969af55
Ojas Aklecha <90605717+ojasaklechayt@users.noreply.github.com>
2023-07-16 17:37:58 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -32,9 +32,7 @@ function createtable(csvData) {
     let tableHTML = "<table class='table' id='mytable'>";
     tableHTML += "<thead><tr>";
     for (let i = 0; i < numcols + 2; i++) {
-        if (i === 0) {
-            tableHTML += `<th>${headerRow[i]}</th>`;
-        } else if (i === numcols + 1) {
+        if (i === numcols + 1) {
             tableHTML += `<th>Total</th>`;
         } else {
             tableHTML += `<th>${headerRow[i]}</th>`;
@@ -49,10 +47,10 @@ function createtable(csvData) {
         console.log(cols);
         let rowSum = 0;
         tableHTML += "<tr>";
-        tableHTML += `<td contentEditable>${i}</td>`;
+        tableHTML += `<td>${i}</td>`;
         for (let j = 0; j < numcols; j++) {
-            tableHTML += `<td contentEditable>${cols[j]}</td>`;
-            rowSum += parseFloat(cols[j]);
+            tableHTML += `<td><input type="text" value="${cols[j]}" style="width: 50px; border:2px none;" /></td>`;
+            rowSum += parseInt(cols[j], 10);
         }
         tableHTML += `<td>${rowSum.toFixed(2)}</td>`;
         rowtotal += rowSum;
@@ -74,69 +72,76 @@ function createtable(csvData) {
     const deleteRowBtn = document.getElementById("delete-row-btn");
     deleteRowBtn.addEventListener("click", deleteRow);
 
-    const cells = document.querySelectorAll('td[contentEditable="true"]');
-    cells.forEach(cell => {
-        cell.addEventListener("input", updateTotal);
+    const inputs = document.querySelectorAll('input[type="text"]');
+    inputs.forEach(input => {
+        input.addEventListener("blur", updateTotal);
     });
 
     function addRow() {
         const tableBody = document.querySelector("tbody");
         const numRows = tableBody.querySelectorAll("tr").length;
         const index = parseInt(document.getElementById("custom-number-input").value) - 1;
-        if (index > numRows - 1 || index < 0) {
+        if (index > numRows - 1) {
             alert("Invalid index");
             return;
         }
         const newRow = document.createElement("tr");
         const newSerialNumberCell = document.createElement("td");
-        newSerialNumberCell.setAttribute("contentEditable", "true");
-        newSerialNumberCell.textContent = index + 1;
+        newSerialNumberCell.textContent = index + 2;
         newRow.appendChild(newSerialNumberCell);
-
+    
         for (let i = 0; i < numcols; i++) {
             const newCell = document.createElement("td");
-            newCell.setAttribute("contentEditable", "true");
-            newCell.textContent = "1";
+            const newInput = document.createElement("input");
+            newInput.type = "text";
+            newInput.value = "1";
+            newInput.style.width = "50px";
+            newInput.style.border = "none";
+            newInput.addEventListener("blur", updateTotal);
+            newCell.appendChild(newInput);
             newRow.appendChild(newCell);
         }
         const newTotalCell = document.createElement("td");
         newTotalCell.textContent = "0";
         newRow.appendChild(newTotalCell);
-
+    
         if (index === numRows - 1) {
             tableBody.appendChild(newRow);
         } else {
             const refNode = tableBody.querySelectorAll("tr")[index + 1];
             tableBody.insertBefore(newRow, refNode);
+            const rows = tableBody.querySelectorAll("tr");
             for (let i = index + 1; i < numRows + 1; i++) {
-                const serialNumberCell = tableBody.querySelectorAll("tr")[i].querySelectorAll("td")[0];
+                const serialNumberCell = rows[i].querySelectorAll("td")[0];
                 serialNumberCell.textContent = i + 1;
             }
         }
         updateTotal();
     }
-
+    
     function deleteRow() {
         const tableBody = document.querySelector("tbody");
         const numRows = tableBody.querySelectorAll("tr").length;
-
+    
         const index = parseInt(document.getElementById("custom-number-input-two").value) - 1;
-
+    
         if (isNaN(index) || index < 0 || index >= numRows) {
             alert("Invalid index");
             return;
         }
-
+    
         const rowToRemove = tableBody.querySelectorAll("tr")[index];
         tableBody.removeChild(rowToRemove);
-
+    
+        const rows = tableBody.querySelectorAll("tr");
         for (let i = index; i < numRows - 1; i++) {
-            const serialNumberCell = tableBody.querySelectorAll("tr")[i].querySelectorAll("td")[0];
+            const serialNumberCell = rows[i].querySelectorAll("td")[0];
             serialNumberCell.textContent = i + 1;
         }
-
+    
         updateTotal();
     }
+    
 
     function updateTotal() {
         const rows = document.querySelectorAll("tbody tr");
@@ -145,22 +150,37 @@ function createtable(csvData) {
             let rowSum = 0;
             let containsAlphabets = false;
             for (let i = 1; i < cells.length - 1; i++) {
-                const cellValue = cells[i].textContent.trim();
-                if (!isNaN(cellValue) && cellValue !== "") {
-                    rowSum += parseFloat(cellValue);
+                const input = cells[i].querySelector("input");
+                const inputValue = input.value.trim();
+                if (!isNaN(inputValue) && inputValue !== "") {
+                    rowSum += parseInt(inputValue, 10);
+                    cells[i].classList.remove("non-numeric");
+                    input.removeEventListener("input", showAlertOnBlur);
                 } else {
                     containsAlphabets = true;
                     cells[i].classList.add("non-numeric");
-                    cells[i].onfocus = function () {
-                        this.classList.remove("non-numeric");
-                    };
+                    input.addEventListener("input", showAlertOnBlur);
                 }
             }
             cells[cells.length - 1].textContent = rowSum.toFixed(2);
             if (containsAlphabets) {
-                alert("Non-numeric values in the Cell");
+                row.classList.add("non-numeric-row");
+            } else {
+                row.classList.remove("non-numeric-row");
             }
         });
+    }
+
+    function showAlertOnBlur(event) {
+        const input = event.target;
+        const inputValue = input.value.trim();
+        if (!isNaN(inputValue) && inputValue !== "") {
+            input.classList.remove("non-numeric");
+            input.removeEventListener("blur", showAlertOnBlur);
+        } else {
+            input.classList.add("non-numeric");
+            input.style.backgroundColor = "";
+        }
     }
 
     const updateTotalBtn = document.getElementById("update-total-btn");
@@ -174,7 +194,7 @@ function exportCSV() {
     const rows = Array.from(table.querySelectorAll("tr"));
     const headerCells = Array.from(rows[0].querySelectorAll("th"));
     const headerRow = headerCells.map(cell => cell.textContent).join(",");
-    const dataCells = rows.slice(1).map(row => Array.from(row.querySelectorAll("td")).map(td => td.textContent));
+    const dataCells = rows.slice(1).map(row => Array.from(row.querySelectorAll("td")).map(td => td.querySelector("input").value));
     const dataRows = dataCells.map(row => row.join(","));
     const data = [headerRow, ...dataRows].join("\n");
     const dataUri = "data:text/csv;charset=utf-8," + encodeURIComponent(data);
@@ -190,7 +210,7 @@ function exportJSON() {
     const table = document.querySelector("table");
     const rows = Array.from(table.querySelectorAll("tr"));
     const headerCells = Array.from(rows[0].querySelectorAll("th"));
-    const dataCells = rows.slice(1).map(row => Array.from(row.querySelectorAll("td")).map(td => td.textContent));
+    const dataCells = rows.slice(1).map(row => Array.from(row.querySelectorAll("td")).map(td => td.querySelector("input").value));
     const data = dataCells.map(row => Object.fromEntries(headerCells.map((cell, i) => [cell.textContent, row[i]])));
     const jsonData = JSON.stringify(data, null, null);
     const dataUri = "data:text/json;charset=utf-8," + encodeURIComponent(jsonData);
